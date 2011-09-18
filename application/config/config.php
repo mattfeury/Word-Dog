e828629350cc13fa80c0ccc2d0391b8a339729fc
Mike Hirth <hirthy@gmail.com>
2011-09-18 18:15:52 -0400

    Added basic login with minimal validation. Register and login are on the same page, markup is generated in the controller. No seperate view yet.

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,8 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$hash = sha1('the sauce boss');
+$config['encryption_key'] = $hash;
 
 /*
 |--------------------------------------------------------------------------
