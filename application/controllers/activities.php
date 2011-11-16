a6d0dac9f53756383cca4a1ce67daec2e6d2f533
Matt Feury <mattfeury@gmail.com>
2011-11-15 22:05:41 -0500

    Added levels to activities

--- application/controllers/activities.php
+++ application/controllers/activities.php
@@ -3,12 +3,30 @@
 class Activities extends CI_Controller {
 
   public static $activitiesById = array(
-    0 => 'write_sentence',
-    1 => 'answer_questions',
-    2 => 'memory',
-    3 => 'fill_blank',
-    4 => 'jumble',
-    5 => 'multiple_choice'
+    0 => array(
+          'id' => 'write_sentence',
+          'name' => 'Write Sentence',
+          'numLevels' => 3),
+    1 => array(
+          'id' => 'answer_questions',
+          'name' => 'Answer Questions',
+          'numLevels' => 3),
+    2 => array(
+          'id' => 'memory',
+          'name' => 'Memory',
+          'numLevels' => 3),
+    3 => array(
+          'id' => 'fill_blank',
+          'name' => 'Fill in the Blank',
+          'numLevels' => 3),
+    4 => array(
+          'id' => 'jumble',
+          'name' => 'Word Jumble',
+          'numLevels' => 3),
+    5 => array(
+          'id' => 'multiple_choice',
+          'name' => 'Multiple Choice',
+          'numLevels' => 3)
   );
 
   public function index() {
@@ -26,19 +44,21 @@ class Activities extends CI_Controller {
 		$this->load->view('activity', $data);
   }
 	
-	public function play($activity, $unitId) {
+	public function play($activity, $unitId, $level = 1) {
     $unit = new Unit();
     $unit->id = $unitId;
     $unit->validate()->get();
-    //$unit->lessons->get();
+    
+    // Get a "pruned" version of the object to encode as JSON
+    // the DataMapper adds all sorts of helper fields so we don't want to encode those
     $pruned = $unit->pruned();
 
     $data = array();
-    //print_r($pruned);
     $data['unit_json'] = json_encode($pruned);
+    $data['level'] = $level;
 
     $activityView = self::$activitiesById[$activity];
  
-		$this->load->view($activityView, $data);
+		$this->load->view($activityView['id'], $data);
 	}
 }
