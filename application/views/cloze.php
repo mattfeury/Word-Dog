ea610097221394fb47232f0d9b02086ea03dd7d1
Mike Hirth <hirthy@gmail.com>
2011-12-11 15:34:41 -0500

    Finished printing for cloze for non memory activities.

--- application/views/cloze.php
+++ application/views/cloze.php
@@ -20,7 +20,9 @@
 // Callback for renderNextLesson()
 
 var numChoices = 3; //number of choices to display.
-function defineActivityForLesson(lesson) {
+function defineActivityForLesson(lesson, $target) {
+  var forPrint = ! $target ? false : true;
+  $target = ! $target ? $('#lesson') : $target;
   // Remove a random word from the sentence. Replace with button
   var sentence = lesson.sentence,
       clozeHtml = createCloze(sentence, 1);
@@ -28,12 +30,12 @@ function defineActivityForLesson(lesson) {
   var missingWord = $(clozeHtml).find('.answer').attr('data-answer');
 
   // Replace image, sentence with missing word, and word choices (if we need to)
-  $('#lesson')
+  $target
     .find('.sentence')
       .html(clozeHtml);
 
   if (config.displayPicture) {
-    $('#lesson')
+    $target
       .find('.picture')
         .attr('src', BASE_SRC + 'uploads/' + lesson['image'])
       .end();
@@ -47,12 +49,17 @@ function defineActivityForLesson(lesson) {
       if (otherLessons.length)
         choices.push(otherLessons.shift().sentence.split(/\W+/).removeWhere('').sort(function() {return 0.5 - Math.random()}).pop());
 
-    $('#lesson .choices').empty();
+    $target.find('.choices').empty();
     $.each(choices, function(i, choice) {
-      $('#lesson .choices').append(
+      $target.find('.choices').append(
         $('<li/>').text(choice)
       )
     });
+  } 
+  // Remove button and inputs when printing
+  if(forPrint) { 
+    // Can't give width to a span, must float div instead
+    $target.find('.missing').replaceWith('<span class="blank"></span>');
   }
 }
 
@@ -76,6 +83,39 @@ $(document).ready(function(){
   //load first lesson
   renderNextLesson();
 
+  $('.cover').live('click', function() {
+    $(this)
+      .closest('.missing')
+        .addClass('guessing')
+        .find('input')
+          .focus();
+  })
+  //specify html for printing for every lesson in the unit
+  if(isPrint){
+    var $print = $('<div/>')
+      .append('<h1>' + $('h1').text() + '</h1>')
+      .append('<h2>Find the missing word from the choices below.</h2>');
+    $.each(unit.lessons, function(i, lesson) {
+      var $template = $('<div><img class="picture" /><div class="sentence"></div><ul class="choices"></ul></div>');
+      defineActivityForLesson(lesson, $template);
+      $print.append($template.html());        
+    });
+    // Create bank
+    $print.append('<ul class="bank"></ul>');
+    $allChoices = $print.find('.choices').find('li');
+    $print.find('.bank').append($allChoices);
+    // Remove duplicates
+    var seen = {};
+    $print.find('li').each(function() {
+        var txt = $(this).text();
+        if (seen[txt])
+            $print.find(this).remove();
+        else
+            seen[txt] = true;
+    });
+    
+    printActivity($print.html());
+  }
   $('.answer').live('keypress', function(e) {
     if(e.which == 13) {
       checkAnswer($(this));
